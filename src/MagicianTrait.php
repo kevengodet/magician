fea4ed687f819ec8858a787917e96f0ece41f35c
Keven <keven.mail@gmail.com>
2017-11-29 16:56:33 +0100

    Initial commit

---
+++ src/MagicianTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace GK\Magician;
+
+trait MagicianTrait
+{
+    public function __get($name)
+    {
+        foreach ((new \ReflectionObject($this))->getMethods() as $method) {
+            /* @var $method \ReflectionMethod */
+            if (strpos($method->getName(), '__magicGet') === 0) {
+                $v = $method->invoke($this, $name);
+                if (!is_null($v)) {
+                    return $v;
+                }
+            }
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        foreach ((new \ReflectionObject($this))->getMethods() as $method) {
+            /* @var $method \ReflectionMethod */
+            if (strpos($method->getName(), '__magicSet') === 0) {
+                $v = $method->invoke($this, $name, $value);
+            }
+        }
+    }
+
+    public function __call($methodName, $arguments)
+    {
+        $return = null;
+
+        foreach ((new \ReflectionObject($this))->getMethods() as $method) {
+            /* @var $method \ReflectionMethod */
+            if (strpos($method->getName(), '__magicCall') === 0) {
+                $v = $method->invoke($this, $methodName, $arguments);
+                if (!is_null($v)) {
+                    $return = $v;
+                }
+            }
+        }
+
+        return $return;
+    }
+}
